3d3eed37f9f5feaed4cadb04ce52af5affc4609f
florian.weidner <florian.weidner@orca-software.com>
2023-05-10 08:37:30 +0200

    last sections

---
+++ src/thesis/listings/MaterialClassification.cs
@@ -0,0 +1,17 @@
+// Load your data
+var materialsDv = mlContext.Data.LoadFromEnumerable(trainable);
+
+//Define your training pipeline
+var pipeline =
+    mlContext.Transforms.Conversion.MapValueToKey("Label", "MaterialCategory")
+        .Append(mlContext.Transforms.Text.FeaturizeText("MaterialBezeichnungFeaturized", options, "MaterialBezeichnung"))
+        .AppendCacheCheckpoint(mlContext)
+        .Append(mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy(featureColumnName: "MaterialBezeichnungFeaturized", labelColumnName: "Label"))
+        .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+// Train and save the model
+var model = pipeline3.Fit(split.TrainSet);
+mlContext.Model.Save(model, materialsDv.Schema, MODEL_PATH);
+//Evaluate the model
+var testdata = model.Transform(split.TestSet);
+var evaluation = mlContext.MulticlassClassification.Evaluate(testdata);
